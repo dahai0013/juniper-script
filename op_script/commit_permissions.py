e965cd8d5cebbea46ba9ac9f2c293353d6e18bd8
jean-luc KRIKER <dahai0013@gmail.com>
2018-10-18 12:34:22 +0100

    1 version

--- op_script/commit_permissions.py
+++ op_script/commit_permissions.py
@@ -2,3 +2,16 @@ from junos import Junos
 from junos import Junos_Context
 from junos import Junos_Configuration
 import jcs
+
+def main():
+    # Get configuration root object
+    root = Junos_Configuration
+
+    message = "Permission all is assigned to invalid class."
+
+    # Warn about any login classes with the all permission set
+    for element in root.findall("./system/login/class[permissions='all']"):
+        jcs.emit_warning("class:" + element.find('name').text + " " + message)
+
+if __name__ == '__main__':
+    main()
